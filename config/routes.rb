85d311421e7a0f41a9b48ba3d73224df4d1ea304
tmartins1 <tiagomartins282@gmail.com>
2017-11-13 04:27:12 +0000

    added simple token authentication, jbuilder and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :users
-  namespace :v1, defaults: { format: :json } do
+  namespace :v1, defaults: {format: :json} do
     resources :users
+    resources :sessions, only: [:create, :destroy]
   end
 end
