c842e9ed54d6124cb0c2f339d51fbc50abc7a5b7
tmartins1 <tiagomartins@Tiagos-Air.lan>
2018-02-11 22:39:37 +0000

    adding email to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
   it { should validate_length_of(:username).is_at_least(4).is_at_most(255) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_length_of(:email).is_at_least(4).is_at_most(255) }
+  it { should allow_value(/\A\S+@.+\.\S+\z/).for(:email) }
   it { should validate_presence_of(:password) }
   it { should validate_length_of(:password).is_at_least(6).is_at_most(255) }
 
