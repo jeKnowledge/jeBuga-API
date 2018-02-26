41936f4142ad71b13c03dda04975fe306f94305d
tmartins1 <tiagomartins282@gmail.com>
2018-02-26 19:30:44 +0000

    removing files

--- Experience · Resume.io_files/pub_bb5424bfb86ba0c9a5bf.js
+++
@@ -1,5 +0,0 @@
-function rfsnLoadScript(e,t,a){var n=document.createElement("script");return n.type="text/javascript",n.async=0,n.defer=0,n.readyState?n.onreadystatechange=function(){("loaded"==n.readyState||"complete"==n.readyState)&&(n.onreadystatechange=null,t())}:n.onload=function(){"undefined"!=typeof t&&t()},n.src=e,a?(o=document.getElementsByTagName("script")[0],o.parentNode.insertBefore(n,o)):document.getElementsByTagName("head")[0].appendChild(n),n};
-
-
-var _refersion=function(a){"undefined"==typeof a&&(a=function(){});rfsnLoadScript("https://resumeio.refersion.com/tracker/v3/merchant/pub_bb5424bfb86ba0c9a5bf.js?v="+Math.floor(100*Math.random()),a)};
-
